20f93520113e8a98a4b2997e84bd50076924ea0f
sakthi04power <52461885+sakthi04power@users.noreply.github.com>
2019-08-20 09:21:06 +0530

    Update main.php

--- main.php
+++ main.php
@@ -1,1 +1,46 @@
-
+<?php
+//TEST FOR "Falabella Corporate Services"
+class Main
+{
+  public $rmindr3;
+  public $rmindr5;
+  public $rmindr35;
+  public $num;
+  
+public  function index(){
+      for($i =1; $i <=100;$i++)
+      {
+          $this->num = $i;
+          $this->rmindr3  = $this->getremin($i,3);
+          $this->rmindr5  = $this->getremin($i,5);
+          $this->rmindr35 = $this->sumremin($this->rmindr3,$this->rmindr5);
+          $this->printresult();
+      }
+  }
+ public function getremin($count,$val )
+    {
+        return $count % $val;
+    }
+ public function sumremin($numthree,$numfive)
+    {
+        return $numthree +  $numfive;
+    }
+  
+ public function printresult()
+    {
+        switch (true) {
+            case ($this->rmindr35 == 0 ):
+                echo "Linianos";
+                break;
+            case ($this->rmindr5 == 0):
+                echo "IT";
+                break;
+            case ($this->rmindr3==0):
+                echo "Linio";
+                break;
+            default:
+                echo $this->num;
+        }
+    }
+}
+?>
